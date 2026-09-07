98aa8981c951ff629a67eccfdd67179fe4db504c
georgiusadibrata <zapypro@gmail.com>
2026-09-07 18:15:40 +0700

    Done with tutorial 2

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Georgius Satria Adibrata",
+        "npm": "2506589976",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "I'm Geo, a CS Student in Universitas Indonesia."
+            "Currently developing my programming skills and I will take any opportunity to improve my abilities!"
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Georgius Satria Adibrata",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
